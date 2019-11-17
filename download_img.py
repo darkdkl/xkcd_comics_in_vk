03d90a5a21c7cc4e5eca54be94464a0763516773
Dark_Dmake <dark.dmake@gmail.com>
2019-11-17 10:08:01 +0300

    lower_case

--- download_img.py
+++ download_img.py
@@ -3,11 +3,11 @@ import os
 
 
 def download_image(url, name):
-    IMAGE_DIR = os.path.normcase('.')
-    os.makedirs(IMAGE_DIR, exist_ok=True)
+    image_dir = os.path.normcase('.')
+    os.makedirs(image_dir, exist_ok=True)
     img_response = requests.get(url)
     exstension = url.rsplit('.')[-1]
-    fname = f'{IMAGE_DIR}/{name}.{exstension}'
+    fname = f'{image_dir}/{name}.{exstension}'
     with open(fname, 'wb') as img:
         img.write(img_response.content)
     return fname
